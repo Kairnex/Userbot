62c5a3e49d80b7baeb5dbe594b059e28bfd38407
Kairnex <codexkairnex@gmail.com>
2025-07-10 13:37:19 +0530

    Create Session_manager.py

---
+++ Session_manager.py
@@ -0,0 +1,19 @@
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+from pymongo import MongoClient
+from config import MONGO_URI, SESSION_COLLECTION, API_ID, API_HASH
+
+client = MongoClient(MONGO_URI)
+db = client.get_database()
+col = db[SESSION_COLLECTION]
+
+def save_session_string(session_string: str):
+    if not col.find_one({"session": session_string}):
+        col.insert_one({"session": session_string})
+
+def get_all_sessions():
+    return [StringSession(doc["session"]) for doc in col.find()]
+
+def get_session_clients():
+    return [TelegramClient(session, API_ID, API_HASH) for session in get_all_sessions()]
+  
