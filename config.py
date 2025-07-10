d303fd2d790c6b7b60e091d9d5e716252aa75df3
Kairnex <codexkairnex@gmail.com>
2025-07-10 13:54:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-MONGO_URI = os.environ.get("MONGO_URI")  # For storing string sessions
+API_ID = int(os.environ.get("API_ID","9696783"))
+API_HASH = os.environ.get("API_HASH","3e74a9830493e9261410a947428dbb34")
+BOT_TOKEN = os.environ.get("BOT_TOKEN","8111191177:AAHIm-L6LDVVKTvIaFP1CiB9pyLDXc_n5HY")
+MONGO_URI = os.environ.get("MONGO_URI","mongodb+srv://codexkairnex:gm6xSxXfRkusMIug@cluster0.bplk1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # For storing string sessions
 
 SESSION_COLLECTION = "string_sessions"
